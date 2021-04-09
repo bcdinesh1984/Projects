43ea0a8df038a100532ee66b3f594e1708676159
Dinesh Chandran <dinesh.chandran@ukg.com>
2021-04-09 06:35:56 -0400

    completed

---
+++ DemoHttpTrigger/__init__.py
@@ -0,0 +1,26 @@
+import logging
+
+import azure.functions as func
+from . import summ
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    v1 = int(req.params.get('v1'))
+    v2 = int(req.params.get('v2'))
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. sum of {v1} + {v2} = {summ.add(v1,v1)}")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
